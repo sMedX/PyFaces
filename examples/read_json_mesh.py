fffd84c9561f1f327e033885ce4f3fa637e11743
Ruslan N. Kosarev <ruslan.kosarev@gmail.com>
2018-12-17 22:14:24 +0300

    update json example

--- examples/read_json_mesh.py
+++ examples/read_json_mesh.py
@@ -21,20 +21,24 @@ if __name__ == '__main__':
     points = data['vertices'][0]['values']
     number_of_points = int(len(points) / 3)
     points = np.reshape(points, [number_of_points, 3])
+    print('points', points.shape)
 
     # normals
     print(data['vertices'][1]['name'])
     normals = data['vertices'][1]['values']
     normals = np.reshape(normals, [number_of_points, 3])
+    print('normals', normals.shape)
 
     # colors
     print(data['vertices'][2]['name'])
     colors = data['vertices'][2]['values']
     colors = np.reshape(colors, [int(len(colors) / 4), 4])[:, :3]
+    print('colors', colors.shape)
 
     # cells
     print(data['connectivity'][0]['name'])
     cells = data['connectivity'][0]['indices']
     number_of_cells = int(len(cells) / 3)
     cells = np.reshape(cells, [number_of_cells, 3])
+    print('cells', cells.shape)
 
