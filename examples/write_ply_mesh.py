5d793b9d30f6a2ceb1385fdd5cdb1f21a2a1f6ba
korus <kosarev@smedx.com>
2018-12-25 23:39:55 +0300

    example write ply mesh has been implemented

---
+++ examples/write_ply_mesh.py
@@ -0,0 +1,33 @@
+__author__ = 'Ruslan N. Kosarev'
+
+import os
+import numpy as np
+from plyfile import PlyData, PlyElement
+from examples import config
+from tffaces.models import FaceModel
+import itertools
+
+# ======================================================================================================================
+if __name__ == '__main__':
+
+    # read model face
+    model = FaceModel(filename=config.bfm2017face12nomouth)
+    points = model.shape.points
+    colors = model.color.colors
+    print(model)
+
+    # initialize vertexes
+    vertexes = [tuple(itertools.chain(*t, [255])) for t in zip(points, 255*colors)]
+    vertexes = np.array(vertexes, dtype=[('x', 'f4'), ('y', 'f4'), ('z', 'f4'),
+                                         ('red', 'u1'), ('green', 'u1'), ('blue', 'u1'), ('alpha', 'u1')])
+    vertexes = PlyElement.describe(vertexes, 'vertex')
+
+    # initialize faces
+    faces = [(f,) for f in np.transpose(model.shape.representer.np_cells)]
+    faces = np.array(faces, dtype=[('vertex_indices', 'i4', (3,))])
+    faces = PlyElement.describe(faces, 'face')
+
+    # write output ply file
+    filename = 'mean_face.ply'
+    filename = os.path.join(config.outdir, os.path.basename(filename))
+    PlyData((vertexes, faces), text=True).write(filename)
